7216f938fcb7fe4ea8ad4124441eab56fe030e78
Mehmetali Shaqiri <m.shaqiri@spartansoft.org>
2011-07-29 18:21:57 +0200

    Added some global constants used across Beethoven.

---
+++ src/Beethoven/Beethoven.Plugins/Shared/GlobalConstants.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Beethoven - A lightweight framework for building plugin based 
+ * web applications using Asp.Net MVC and MEF.
+ * 
+ * Written for .NET 4.0 in C#
+ * 
+ * Copyright (C) 2009, 2010, 2011. All Right Reserved, Spartansoft L.L.C.
+ * http://spartansoft.org
+ * 
+ * Proprietary and Confidential information of Spartansoft L.L.C. 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * without the authorization of Spartansoft L.L.C.  is prohibited. * 
+ * 
+ * "Whatever happens SPARTAN's code must stand ... or at least crash responsibly."
+ *  
+ * File Name: GlobalConstants.cs
+ * 
+ * File Authors:
+ * 		Mehmetali N. Shaqiri, m.shaqiri@spartansoft.org
+ * 
+ * Date Created:
+ *      29.07.2011 06:08 PM
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beethoven.Plugins.Shared
+{
+    public static class GlobalConstants
+    {
+        public const string Bin = "bin";
+
+
+        public const string Plugins = "Plugins";
+
+        public const string ControllerInRouteData = "controller";
+    }
+}
