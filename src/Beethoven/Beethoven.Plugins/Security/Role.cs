1ae7b18a80f99639bee822d0fb0f5a4070bd1121
Mehmetali Shaqiri <m.shaqiri@spartansoft.org>
2011-08-22 19:47:54 +0200

    Role POCO

---
+++ src/Beethoven/Beethoven.Plugins/Security/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beethoven.Plugins.Security
+{
+    public class Role
+    {
+        public Guid ApplicationId { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public string Name { get; set; }
+
+        public string LoweredRoleName { get; set; }
+
+        public string  Description { get; set; }
+
+        public virtual ICollection<Capability> Capabilities { get; set; }
+    }
+}
