b583182279c1850de9df5a1207e5b549b01a6c33
Mehmetali Shaqiri <m.shaqiri@spartansoft.org>
2011-08-22 19:46:56 +0200

    Used in plugins to expose their own capabilities to the host.

---
+++ src/Beethoven/Beethoven.Plugins/Security/Capability.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Beethoven - A lightweight framework for building plugin based 
+ * web applications using Asp.Net MVC and MEF.
+ * 
+ * Written for .NET 4.0 in C#
+ * 
+ * Copyright (C) 2009, 2010, 2011. All Right Reserved, Spartansoft L.L.C.
+ * http://spartansoft.org
+ * 
+ * Proprietary and Confidential information of Spartansoft L.L.C. 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * without the authorization of Spartansoft L.L.C.  is prohibited. * 
+ * 
+ * "Whatever happens SPARTAN's code must stand ... or at least crash responsibly."
+ *  
+ * File Name: Capability.cs
+ * 
+ * File Authors:
+ * 		Mehmetali N. Shaqiri, m.shaqiri@spartansoft.org
+ * 
+ * Date Created:
+ *      03.08.2011 08:52 PM
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Beethoven.Plugins.Security
+{
+    [Serializable]
+    [XmlRoot("Capability")]
+    public class Capability
+    {
+        public string ID { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
---
+++ src/Beethoven/Beethoven.Plugins/Security/CapabilityRegistration.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Beethoven - A lightweight framework for building plugin based 
+ * web applications using Asp.Net MVC and MEF.
+ * 
+ * Written for .NET 4.0 in C#
+ * 
+ * Copyright (C) 2009, 2010, 2011. All Right Reserved, Spartansoft L.L.C.
+ * http://spartansoft.org
+ * 
+ * Proprietary and Confidential information of Spartansoft L.L.C. 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * without the authorization of Spartansoft L.L.C.  is prohibited. * 
+ * 
+ * "Whatever happens SPARTAN's code must stand ... or at least crash responsibly."
+ *  
+ * File Name: CapabilityRegistration.cs
+ * 
+ * File Authors:
+ * 		Mehmetali N. Shaqiri, m.shaqiri@spartansoft.org
+ * 
+ * Date Created:
+ *      03.08.2011 10:40 PM
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beethoven.Plugins.Security
+{
+    public abstract class CapabilityRegistration
+    {
+        public abstract List<Capability> Capabilities { get; }
+    }
+}
