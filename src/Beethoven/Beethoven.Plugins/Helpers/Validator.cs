2750d8d38cdf15cfa14debe04817eb64af849795
Mehmetali Shaqiri <m.shaqiri@spartansoft.org>
2011-08-27 18:14:48 +0200

    Added validators for email and url's

---
+++ src/Beethoven/Beethoven.Plugins/Helpers/Validator.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Beethoven.Plugins
+ * 
+ * Written for .NET 4.0 in C#
+ * 
+ * Copyright (C) 2009, 2010, 2011. All Right Reserved, Spartansoft L.L.C.
+ * http://spartansoft.org
+ * 
+ * Proprietary and Confidential information of Spartansoft L.L.C. 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * without the authorization of Spartansoft L.L.C.  is prohibited. * 
+ * 
+ * "Whatever happens SPARTAN's code must stand ... or at least crash responsibly."
+ *  
+ * File Name: Validator.cs
+ * 
+ * File Authors:
+ * 		Mehmetali N. Shaqiri, m.shaqiri@spartansoft.org
+ * 
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DeepThought.Core.Helpers
+{
+    /// <summary>
+    /// Validate data entry
+    /// </summary>
+    public class Validator
+    {
+        /// <summary>
+        /// Validate email address
+        /// </summary>
+        /// <param name="Email">Email address to be validated</param>        
+        public static bool IsEmail(string Email)
+        {
+            string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            Regex re = new Regex(strRegex);
+            if (re.IsMatch(Email))
+                return (true);
+            else
+                return (false);
+        }
+
+        /// <summary>
+        /// Validate url
+        /// </summary>
+        /// <param name="Email">url to be validated</param> 
+        public static bool IsUrl(string Url)
+        {
+            string strRegex = "^(https?://)"
+            + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //user@ 
+            + @"(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP- 199.194.52.184 
+            + "|" // allows either IP or domain 
+            + @"([0-9a-z_!~*'()-]+\.)*" // tertiary domain(s)- www. 
+            + @"([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // second level domain 
+            + "[a-z]{2,6})" // first level domain- .com or .museum 
+            + "(:[0-9]{1,4})?" // port number- :80 
+            + "((/?)|" // a slash isn't required if there is no file name 
+            + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+            Regex re = new Regex(strRegex);
+
+            if (re.IsMatch(Url))
+                return (true);
+            else
+                return (false);
+        } 
+    }
+}
