37f09bbb3550e6c91f38ce54163573a930a9c7f9
Mehmetali Shaqiri <m.shaqiri@spartansoft.org>
2011-09-01 04:01:10 +0200

    Generic Viewmodels for lists

---
+++ src/Beethoven/Beethoven.Plugins/ViewModels/IBaseEntityListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Beethoven.Plugins.ViewModels
+{
+   
+    public interface IBaseEntityListViewModel<T>
+    {
+        int Total { get; set; }
+
+        List<T> Items { get; set; }
+    }
+}
